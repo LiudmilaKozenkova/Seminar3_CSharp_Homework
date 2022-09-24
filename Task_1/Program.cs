0a5493482dafc5edfb8e55805670d0f690dc7268
Liudmila <to_shagalova_lu@mail.ru>
2022-09-24 16:48:05 +0500

    Task 1 change

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,25 +1,28 @@
 ﻿int count = 5;
-int[]array = new int[5];
+int[] array = { 1, 2, 8, 2, 1 };
 int index = 0;
+Console.Write("Является ли палиндромом число = ");
+Console.Write(array);
 
-while(index<count)
+while (index < count)
 {
     array[index] = new Random().Next(1, 10);
     Console.Write(array[index] + " ");
-    index ++;
+    index++;
 }
 index = 0;
-if(array[0]==array[3])
-    {if(array[1]==array[4])
+if (array[0] == array[4])
+{
+    if (array[1] == array[3])
     {
-    Console.WriteLine("ДА");
-    } 
-    else 
+        Console.WriteLine("ДА");
+    }
+    else
     {
-      Console.WriteLine("НЕТ");  
+        Console.WriteLine("НЕТ");
     }
-    }
-else 
+}
+else
 {
     Console.WriteLine("НЕТ");
 }
