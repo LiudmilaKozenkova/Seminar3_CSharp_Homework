2f4dbec0a38402da6c3e4c9bac59f00ac4170404
Liudmila <to_shagalova_lu@mail.ru>
2022-09-24 16:32:25 +0500

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 5;
+int[]array = new int[5];
+int index = 0;
+
+while(index<count)
+{
+    array[index] = new Random().Next(1, 10);
+    Console.Write(array[index] + " ");
+    index ++;
+}
+index = 0;
+if(array[0]==array[3])
+    {if(array[1]==array[4])
+    {
+    Console.WriteLine("ДА");
+    } 
+    else 
+    {
+      Console.WriteLine("НЕТ");  
+    }
+    }
+else 
+{
+    Console.WriteLine("НЕТ");
+}
