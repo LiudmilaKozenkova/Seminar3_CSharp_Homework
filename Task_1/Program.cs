b66dd84796307d47745ccc804562c81cf4fb3b42
Liudmila <to_shagalova_lu@mail.ru>
2022-09-25 16:56:10 +0500

    task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,28 +1,72 @@
-﻿int count = 5;
-int[] array = { 1, 2, 8, 2, 1 };
-int index = 0;
-Console.Write("Является ли палиндромом число = ");
-Console.Write(array);
+﻿//int count = 5;
+//int[] array = { 1, 4, 8, 4, 1 };
+//int index = 0;
+//Console.Write("Является ли палиндромом число = ");
+//Console.Write(array);
+//Console.WriteLine();
 
-while (index < count)
+//1.Функция получения случайных чисел от мин до макс
+int GetIntValue(int min, int max)
 {
-    array[index] = new Random().Next(1, 10);
-    Console.Write(array[index] + " ");
-    index++;
+    return new Random().Next(min, max);
+    //int value = new Random().Next(1, 10);
+    //return value;
 }
-index = 0;
-if (array[0] == array[4])
+//2.Метод создания нового массива
+int[] CreateNewArray(int size)
 {
-    if (array[1] == array[3])
+    return new int[size];
+    //int[]array = new int[size];
+    //return array;
+}
+//3.Метод заполнения массива
+void Fill(int[] array)
+{
+    int count = array.Length;
+    int index = 0;
+
+    while (index < count)
     {
-        Console.WriteLine("ДА");
+        array[index] = GetIntValue(1, 10);
+        index++;
+    }
+}
+
+//5.Распечатать исходный массив
+void Print(int[]array)
+{
+    int i =0;
+    int size = array.Length;
+    while(i<size)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+Console.WriteLine(); 
+}
+//4.Метод определения, является ли число палиндромом
+void Palindrome (int[] array)
+{
+int[] source = CreateNewArray(5);
+    if (array[0] == array[4])
+    {
+        if (array[1] == array[3])
+        {
+            Console.WriteLine("ДА");
+        }
+        else
+        {
+            Console.WriteLine("НЕТ");
+        }
     }
     else
     {
         Console.WriteLine("НЕТ");
     }
 }
-else
-{
-    Console.WriteLine("НЕТ");
-}
+
+int[]collection = CreateNewArray(5);
+Console.Write("Является ли палиндромом число = ");
+Fill(collection);
+Print(collection);
+Palindrome(collection);
